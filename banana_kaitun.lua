bdfc01b8c865a85f5525c8ef9f4b95d3fe327421
clarksye <clarksye@users.noreply.github.com>
2025-06-02 01:23:29 -0700

    Update banana_kaitun.lua

--- banana_kaitun.lua
+++ banana_kaitun.lua
@@ -1,5 +1,5 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().Key = ""
+getgenv().Key = "0c35250991e58f6cd19192bc"
 getgenv().Config = {
     ["Buy Egg"] = {
         ["Select Egg"] = {
