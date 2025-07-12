f7807612f078a3090a0346aaeae436e18651334d
clarksye <clarksye@users.noreply.github.com>
2025-07-12 05:28:55 -0700

    Create pets.lua

---
+++ pets.lua
@@ -0,0 +1,27 @@
+getgenv().Config = {
+    ["Place Eggs"] = {
+        ["Enabled"] = true,
+        ["Order By"] = {
+            "Common Egg", "Common Summer Egg", "Rare Summer Egg", "Legendary Egg", "Night Egg", "Bee Egg", "Mythical Egg", "Paradise Egg", "Bug Egg"
+        },
+        ["Item"] = {
+            ["Common Egg"] = true,
+            ["Common Summer Egg"] = true,
+            ["Rare Summer Egg"] = true,
+            ["Legendary Egg"] = true,
+            ["Night Egg"] = true,
+            ["Bee Egg"] = true,
+            ["Mythical Egg"] = true,
+            ["Paradise Egg"] = true,
+            ["Bug Egg"] = true
+        }
+    },
+    ["Sell Pets"] = {
+        ["Enabled"] = true,
+        ["Level"] = 20, -- Jual semua yang di bawah level
+        ["Keep"] = { -- List pet yang tidak ingin di jual
+            "Dragonfly", "Disco Bee", "Raccoon", "Queen Bee", "Cooked Owl", "T-rex", "Toucan", "Ostrich", "Triceratop", "Brontosaurus", "Capybara", "Starfish", "Blood Owl"
+        }
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alienschub/alienhub/refs/heads/main/pets.lua"))()
