a546fe6903f2b6e7a58c31f2cbf69ad0303acf8a
clarksye <clarksye@users.noreply.github.com>
2025-07-14 20:09:11 -0700

    Update pets.lua

--- pets.lua
+++ pets.lua
@@ -21,7 +21,7 @@ getgenv().Config = {
         ["Enabled"] = true,
         ["Level"] = 45, -- Jual semua yang di bawah level
         ["Keep"] = { -- List pet yang tidak ingin di jual
-            "Dragonfly", "Disco Bee", "Raccoon", "Queen Bee", "Cooked Owl", "T-Rex", "Toucan", "Ostrich", "Triceratops", "Brontosaurus", "Capybara", "Starfish", "Blood Owl", "Bald Eagle", "Moon Cat"
+            "Dragonfly", "Disco Bee", "Raccoon", "Queen Bee", "Cooked Owl", "T-Rex", "Toucan", "Ostrich", "Triceratops", "Brontosaurus", "Capybara", "Starfish", "Blood Owl", "Bald Eagle", "Moon Cat", "Scarlet Macaw", "Blood Hedgehog", "Blood Kiwi", "Butterfly", "Pterodactyl"
         }
     }
 }
