bc70ccf8ab5b00028aa418458be8cd3009bcd57f
clarksye <clarksye@users.noreply.github.com>
2025-07-14 20:17:05 -0700

    Update pets.lua

--- pets.lua
+++ pets.lua
@@ -2,16 +2,20 @@ getgenv().Config = {
     ["Place Eggs"] = {
         ["Enabled"] = true,
         ["Order By"] = {
-            "Common Egg", "Common Summer Egg", "Rare Summer Egg", "Legendary Egg", "Night Egg", "Bee Egg", "Dinosaur Egg", "Mythical Egg", "Paradise Egg", "Bug Egg"
+            "Common Egg", "Common Summer Egg", "Uncommon Egg", "Rare Egg", "Rare Summer Egg", "Legendary Egg", "Night Egg", "Bee Egg", "Dinosaur Egg", "Oasis Egg", "Primal Egg", "Mythical Egg", "Paradise Egg", "Bug Egg"
         },
         ["Item"] = {
             ["Common Egg"] = true,
             ["Common Summer Egg"] = true,
+            ["Uncommon Egg"] = true,
+            ["Rare Egg"] = true,
             ["Rare Summer Egg"] = true,
             ["Legendary Egg"] = true,
             ["Night Egg"] = true,
             ["Bee Egg"] = true,
             ["Dinosaur Egg"] = true,
+            ["Oasis Egg"] = true,
+            ["Primal Egg"] = true,
             ["Mythical Egg"] = true,
             ["Paradise Egg"] = true,
             ["Bug Egg"] = true
@@ -21,7 +25,8 @@ getgenv().Config = {
         ["Enabled"] = true,
         ["Level"] = 45, -- Jual semua yang di bawah level
         ["Keep"] = { -- List pet yang tidak ingin di jual
-            "Dragonfly", "Disco Bee", "Raccoon", "Queen Bee", "Cooked Owl", "T-Rex", "Toucan", "Ostrich", "Triceratops", "Brontosaurus", "Capybara", "Starfish", "Blood Owl", "Bald Eagle", "Moon Cat", "Scarlet Macaw", "Blood Hedgehog", "Blood Kiwi", "Butterfly", "Pterodactyl"
+            "Dragonfly", "Disco Bee", "Raccoon", "Queen Bee", "Cooked Owl", "T-Rex", "Toucan", "Ostrich", "Triceratops", "Brontosaurus", "Capybara", "Starfish", "Blood Owl",
+            "Bald Eagle","Moon Cat", "Scarlet Macaw", "Blood Hedgehog", "Blood Kiwi", "Butterfly", "Pterodactyl"
         }
     }
 }
