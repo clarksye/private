018d415256292a77a15047386dde6c0966086ede
clarksye <clarksye@users.noreply.github.com>
2025-07-19 04:49:08 -0700

    Update anon.lua

--- anon.lua
+++ anon.lua
@@ -368,7 +368,7 @@ RunService.Heartbeat:Connect(function(dt)
         step = 5
         stepInProgress = false
 
-    elseif step == 5 and elapsed >= 204 and not stepInProgress then
+    elseif step == 5 and elapsed >= 205 and not stepInProgress then
         stepInProgress = true
         print("STEP 5: Tanam di Posisi Terbaik")
         local bestPos = getBestPlantingSpot()
