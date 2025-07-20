5713716c472089ad8e77c2e28f1f6aa95fb77b1b
clarksye <clarksye@users.noreply.github.com>
2025-07-19 21:13:47 -0700

    Create removePlant.lua

---
+++ removePlant.lua
@@ -0,0 +1,16 @@
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local removeRemote = game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("Remove_Item")
+
+for _, farm in ipairs(workspace.Farm:GetChildren()) do
+    local sign = farm:FindFirstChild("Sign")
+    if sign and sign:GetAttribute("_owner") == Player.Name 
+        for _, plant in ipairs(farm.Important.Plants_Physical:GetChildren()) do
+            if not table.find({"Candy Blossom", "Moon Blossom", "Sugar Apple", "Sunflower", "Beanstalk", "Giant Pinecone"}, plant.Name) then
+                remote:FireServer(plant:FindFirstChildWhichIsA("BasePart"))
+                task.wait(0.05)
+            end
+        end
+        break
+    end
+end
