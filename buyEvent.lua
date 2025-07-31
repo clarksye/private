633d31cc5d2f48e4f56f0bb343647cf904866de2
clarksye <clarksye@users.noreply.github.com>
2025-07-31 02:52:33 -0700

    Update buyEvent.lua

--- buyEvent.lua
+++ buyEvent.lua
@@ -2,45 +2,20 @@ local DataService = require(game:GetService("ReplicatedStorage"):WaitForChild("M
 
 -- Auto Buy Stock
 task.spawn(function()
-    while task.wait(10) do
+    while task.wait(2) do
         local success, err = pcall(function()
             local data = DataService:GetData()
             if not data or not data.PetEggStock or not data.GearStock or not data.SeedStock then return end
 
-            -- Auto Buy Eggs
-            if data.PetEggStock and data.PetEggStock.Stocks then
-                local remote = game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuyPetEgg")
-                for index, info in ipairs(data.PetEggStock.Stocks) do
-                    if info and info.Stock > 0 then
-                        for i = 1, info.Stock do
-                            remote:FireServer(index)
-                            task.wait()
-                        end
-                    end
-                end
-            end
-
-            -- Auto Buy Tools
-            if data.GearStock and data.GearStock.Stocks then
-                local remote = game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuyGearStock")
-                for itemName, info in pairs(data.GearStock.Stocks) do
-                    if info and info.Stock > 0 then
-                        for _ = 1, info.Stock do
-                            remote:FireServer(itemName)
-                            task.wait()
-                        end
-                    end
-                end
-            end
-
-            -- Auto Buy Seeds
-            if data.SeedStock and data.SeedStock.Stocks then
-                local remote = game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):FindFirstChild("BuySeedStock")
-                for itemName, info in pairs(data.SeedStock.Stocks) do
-                    if info and info.Stock > 0 then
-                        for _ = 1, info.Stock do
-                            remote:FireServer(itemName)
-                            task.wait()
+             -- Auto Buy Event
+            if data.eventShopStock and data.eventShopStock.Stocks then
+                for itemName, info in pairs(data.eventShopStock.Stocks) do
+                    if table.find({"Zen Egg", "Zenflare", "Zen Seed Pack", "Sakura Bush"}, itemName) then
+                        if info and info.Stock > 0 then
+                            for i = 1, info.Stock do
+                                buyEventShopStockRemote:FireServer(itemName)
+                                task.wait()
+                            end
                         end
                     end
                 end
